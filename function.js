def6170d3b9d1a7a0ae064c9ea9cc6816347c20a
Maham Siddiqui <49103875+MahamSiddiqui@users.noreply.github.com>
2019-06-15 18:02:38 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+function changeName() {
+
+    document.querySelector('span').innerHTML = document.querySelector('input').value;;
+
+}
+
+function changeColor() {
+    document.querySelector("h1").style = "color: blue";
+    document.querySelector("h1").style = "color: black";
+    document.querySelector("h1").style = "color: pink";
+    document.querySelector("h1").style = "color: red";
+}
